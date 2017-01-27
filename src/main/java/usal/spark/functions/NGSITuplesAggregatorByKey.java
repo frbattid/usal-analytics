6a4784268e8f452cf3aa97c2514348d04f90aace
frbattid <francisco.romerobueno@telefonica.com>
2017-01-27 15:44:34 +0100

    Add license header to all java files

--- src/main/java/usal/spark/functions/NGSITuplesAggregatorByKey.java
+++ src/main/java/usal/spark/functions/NGSITuplesAggregatorByKey.java
@@ -1,5 +1,19 @@
-/*
- * Some header...
+/**
+ * Copyright 2017 Francisco Romero Bueno
+ *
+ * usal-analytics is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * usal-analytics is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with usal-analytics. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License please raise an issue at
+ * https://github.com/frbattid/usal-analytics
  */
 package usal.spark.functions;
 
